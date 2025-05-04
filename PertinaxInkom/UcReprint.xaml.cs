dbe20c2d953872a46871a5ef637fc5874de8c3ce
Stainllless <servaesrobbertjan@gmail.com>
2025-05-04 23:05:05 +0200

    complete update of UcReprint

--- PertinaxInkom/UcReprint.xaml.cs
+++ PertinaxInkom/UcReprint.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using PertinaxInkom.Properties;
+using Swelio.Engine;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,8 +32,93 @@ namespace PertinaxInkom
 
         private void btnReprint_Click(object sender, RoutedEventArgs e)
         {
+            string Zebraprinter = "";
+            string zebraReturn = "";
 
-            CloseRequested?.Invoke(this, EventArgs.Empty);
+            // Validate UUID
+            clsBarcode clsBarcode = new clsBarcode();
+            clsUserDB userDB = new clsUserDB();
+            clsUser user = userDB.GetUserByUuid(txbwristband.Text);
+
+            if (!clsBarcode.ValidateBarcode(txbwristband.Text))
+            {
+                txterrors.Text = "Not a valid wristband.";
+                return;
+            }
+
+            if (user == null)
+            {
+                txterrors.Text = "User not found.";
+                return;
+            }
+
+            // Create a new UUID
+            string newUuid = clsBarcode.CreateBarcode();
+
+            // Update the user's UUID
+            userDB.UpdateUser(
+                user.Id,
+                user.Address_Id,
+                user.Wallet_Id,
+                user.Nick_Name,
+                user.Password,
+                user.First_Name,
+                user.Last_Name,
+                user.Email,
+                newUuid,
+                user.Birth_Date
+            );
+
+            // Block the old UUID
+            clsBlockedUuidsDB BlockedUuidsDB = new clsBlockedUuidsDB();
+            int? result = BlockedUuidsDB.CreateBlockedUuid(txbwristband.Text);
+            if (result == null)
+            {
+                txterrors.Text = "Something went wrong. Ask admin to add the wristband in the DB.";
+                return;
+            }
+
+            // Get user role
+            clsUserRoleDB userRoleDB = new clsUserRoleDB();
+            ObservableCollection<clsUserRole> userRoles = userRoleDB.GetUserRolesByUser(user.Id);
+            if (userRoles.Count == 0)
+            {
+                txterrors.Text = "User has no assigned role.";
+                return;
+            }
+
+            clsRoleDB roleDB = new clsRoleDB();
+            clsRole role = roleDB.GetRoleById(userRoles[0].RoleId);
+            string zplcode = "";
+
+            // Assign printer and generate ZPL
+            if (role.RoleName == "Admin" || role.RoleName == "Crew")
+            {
+                Zebraprinter = ZebraConfig.Default.IpCrewVolunteerPrinter.ToString();
+                zplcode = clsZebraPrinter.GenerateZPL(ZebraConfig.Default.edition.ToString(), user.Nick_Name, user.First_Name, user.Last_Name, newUuid);
+            }
+            else if (role.RoleName == "Participant")
+            {
+                Zebraprinter = ZebraConfig.Default.IpParticipantPrinter.ToString();
+                zplcode = clsZebraPrinter.GenerateZPL(ZebraConfig.Default.edition.ToString(), user.Nick_Name, user.First_Name, user.Last_Name, newUuid);
+            }
+            else
+            {
+                Zebraprinter = ZebraConfig.Default.IpVisitorPrinter.ToString();
+                zplcode = clsZebraPrinter.GenerateZPL(ZebraConfig.Default.edition.ToString(), "visitor", user.First_Name, user.Last_Name, newUuid);
+            }
+
+            // Send to printer
+            zebraReturn = clsZebraPrinter.SendZPLToPrinter(zplcode, Zebraprinter);
+            if (zebraReturn == "succes")
+            {
+                Thread.Sleep(5000);
+                CloseRequested?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                txterrors.Text = zebraReturn;
+            }
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
